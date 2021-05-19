3a6f74c37d837b1983f08219d4b233cf545d4050
BeastyBlacksmith <BeastyBlacksmith@users.noreply.github.com>
2021-05-19 13:13:15 +0000

    deploy: 2c34782c82ac35f4d6a5fbc4f669df58142b1b12

---
+++ navtreedata.js
@@ -0,0 +1,71 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PyAMA", "index.html", [
+    [ "Packages", "namespaces.html", [
+      [ "PyAMA", "index.html#autotoc_md0", [
+        [ "Installation", "index.html#autotoc_md1", [
+          [ "Anaconda users", "index.html#autotoc_md2", null ],
+          [ "Pip users", "index.html#autotoc_md3", null ],
+          [ "Desktop file installation for Linux users (experimental)", "index.html#autotoc_md4", null ]
+        ] ],
+        [ "Usage", "index.html#autotoc_md5", [
+          [ "Starting PyAMA", "index.html#autotoc_md6", null ],
+          [ "Loading a stack", "index.html#autotoc_md7", null ],
+          [ "Viewing the stack", "index.html#autotoc_md8", null ]
+        ] ]
+      ] ],
+      [ "Packages", "namespaces.html", "namespaces_dup" ],
+      [ "Package Functions", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"",
+"classpyama_1_1io_1_1roi_1_1Roi.html#a926eaf00630646249cb0f422ad49331c",
+"classpyama_1_1roi_1_1base_1_1Roi.html#ae91176e135f53b00a0015e5bf04493db",
+"classpyama_1_1session_1_1view__tk_1_1SessionView__Tk.html#a92ab8ae3856863a6b76cc6c37fd914ee",
+"classpyama_1_1util_1_1status_1_1DummyStatus.html#ad33b571270c4880c0eaefeb326e6007d",
+"stack_2const_8py.html#ac39e081d075960835b7188cd01e3ed35"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
